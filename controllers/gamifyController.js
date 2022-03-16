8eb478b877c0dc903f8490a9e2cd48d9c1c22176
Raul Comeros, Jr <90462635+noice-noise@users.noreply.github.com>
2022-03-16 23:51:18 +0800

    Transfer mammoth parse in gamify controller

--- controllers/gamifyController.js
+++ controllers/gamifyController.js
@@ -1,5 +1,6 @@
-const pdfParse = require("pdf-parse");
 const path = require("path");
+const mammoth = require("mammoth");
+const fs = require("fs");
 
 const gamify_index = (req, res) => {
   res.render("gamify/index", { title: "Gamify", blogs: result });
@@ -43,8 +44,80 @@ const gamify_create_get = (req, res) => {
   res.render("gamify/create", { title: "Gamify" });
 };
 
+const options = {
+  // reserve  H1 for specific page titles for semantic meaning
+  styleMap: ["p[style-name='Heading 1'] => h2:fresh"],
+  styleMap: ["p[style-name='Heading 2'] => h3:fresh"],
+  styleMap: ["p[style-name='Heading 3'] => h4:fresh"],
+  styleMap: ["p[style-name='Heading 4'] => h5:fresh"],
+  styleMap: ["p[style-name='Heading 5'] => h6:fresh"],
+  styleMap: ["p[style-name='Heading 6'] => p:fresh"],
+};
+
+const uploadFolder = path.join(__dirname, "..", "files/gamify");
+const uploadFolderContents = fs.readdirSync(uploadFolder);
+// `${__dirname}/files/sample3.docx`;
+// console.log(files);
+
+const gamify_get_html = (req, res) => {
+  console.log("Hello!");
+
+  // files.foreach((file) => {
+  //   console.log(file.name);
+  // });
+
+  // mammoth
+  //   .convertToHtml(
+  //     { path: `${uploadFolder}/2022-2-C1-MOR-CPEH203-02-RCP-COMEROS.docx` },
+  //     options
+  //   )
+  //   .then(function (result) {
+  //     var html = result.value;
+  //     var messages = result.messages;
+  //     console.log(html);
+  //     console.log(messages);
+  //     res.send(JSON.stringify(html));
+  //   })
+  //   .done();
+  parseAllHtml();
+  // console.log("REC");
+  // console.log(htmlContents);
+  // console.log("----");
+  res.send(JSON.stringify(htmlContents));
+};
+
+var htmlContents = "";
+
+const parseHtml = (docx) => {
+  return mammoth
+    .convertToHtml({ path: `${uploadFolder}/${docx}` }, options)
+    .then(function (result) {
+      var html = result.value;
+      var messages = result.messages;
+      htmlContents += html;
+      // console.log(html);
+      // console.log(messages);
+      return html;
+    });
+};
+
+const parseAllHtml = async () => {
+  for (const file of uploadFolderContents) {
+    var fileInfo = await parseHtml(file);
+    // console.log(fileInfo);
+    // fileInfo.then((data) => {
+    //   console.log(data.json());
+    // });
+    // console.log(fileInfo);
+  }
+
+  console.log("All files have been parsed");
+  // return fileInfo;
+};
+
 module.exports = {
   gamify_index,
   gamify_create_get,
   gamify_file_post,
+  gamify_get_html,
 };
