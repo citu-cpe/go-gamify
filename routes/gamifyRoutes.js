b03179bce584c9636332444bd1b14fa405e73e5b
Raul Comeros, Jr <90462635+noice-noise@users.noreply.github.com>
2022-03-24 22:08:58 +0800

    Add delete file route

--- routes/gamifyRoutes.js
+++ routes/gamifyRoutes.js
@@ -5,6 +5,8 @@ const gamifyController = require("../controllers/gamifyController");
 
 router.get("/", gamifyController.gamify_index);
 router.get("/create", gamifyController.gamify_create_get);
-router.post("/upload", gamifyController.gamify_file_post);
-router.get("/get-html", gamifyController.gamify_get_html);
+router.get("/file", gamifyController.gamify_file_get);
+router.post("/file", gamifyController.gamify_file_post);
+router.delete("/file", gamifyController.gamify_file_delete);
+
 module.exports = router;
