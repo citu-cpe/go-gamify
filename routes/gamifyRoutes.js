cfa29c84ebec6e613658a24ed0d907d0d728067f
Raul Comeros, Jr <90462635+noice-noise@users.noreply.github.com>
2022-03-30 19:33:21 +0800

    Add get uploaded file list route

--- routes/gamifyRoutes.js
+++ routes/gamifyRoutes.js
@@ -8,6 +8,7 @@ router.get("/create", gamifyController.gamify_create_get);
 router.get("/file", gamifyController.gamify_file_get);
 router.post("/file", gamifyController.gamify_file_post);
 router.delete("/file", gamifyController.gamify_file_delete);
+router.get("/file-upload-list", gamifyController.gamify_file_list_get);
 router.post("/save", gamifyController.learning_resource_post);
 
 module.exports = router;
